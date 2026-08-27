422973bca6f30387baf6775a93036a8064b6e67a
roopajubre <roopajubre@gmail.com>
2026-08-27 13:31:26 +0530

    Fix syntax error in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    return {
+  
   base: '/aj-studio-portfolio/',
   plugins: [react(), tailwindcss()],
     resolve: {
